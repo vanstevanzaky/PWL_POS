c5155a9f872f4e546ae84708fe14ec0934b2c73a
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-04-05 15:03:05 +0700

    jbs7- praktikum 1: implentasi authentication langkah 2 fixed

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,19 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updated_at'];
+    protected $fillable = ['username', 'password','nama','level_id', 'created_at', 'updated_at'];
 
     protected $hidden = ['password'];
     protected $casts = ['password' => 'hashed'];
 
-    public function level(){
+    public function level()
+    {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
