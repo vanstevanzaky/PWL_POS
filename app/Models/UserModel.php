793393d6d277cba46e55ee0ab1d1d0e4516420f7
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-04-05 11:38:15 +0700

    jbs7- praktikum 1: implentasi authentication langkah 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,7 +11,10 @@ class UserModel extends Model
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updated_at'];
+
+    protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed'];
 
     public function level(){
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
