e5d9ac4c46099b2be591d139cb0702e82c3da350
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-04-05 19:10:21 +0700

    jbs7- praktikum 2: implentasi authorization di laravel dengan middleware(langkah 1)

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -23,4 +23,11 @@ class UserModel extends Authenticatable
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
+    public function getRoleName(){
+        return $this->level->level_nama;
+    }
+
+    public function hasRole($role){
+        return $this->level->level_kode == $role;
+    }
 }
