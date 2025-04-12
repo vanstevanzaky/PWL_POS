8c3b2d10757174ddced6ca757c82c82cc378f397
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-04-13 00:40:06 +0700

    jbs8- praktikum 1: implentasi upload file untuk import data(modifikasi index barang)

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -4,7 +4,13 @@
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
+                <button onclick="modalAction('{{ route('barang.import') }}')" class="btn btn-sm btn-success mt-1">
+                    Import Barang
+                </button>
                 <a class="btn btn-sm btn-primary mt-1" href="{{ route('barang.create') }}">Tambah</a>
+                <button onclick="modalAction('{{ route('barang.create_ajax') }}')" class="btn btn-sm btn-success mt-1">
+                    Tambah Ajax
+                </button>
             </div>
         </div>
         <div class="card-body">
@@ -20,84 +26,130 @@
                     {{ session('error') }}
                 </div>
             @endif
+            <div id="filter" class="form-horizontal filter-date p-2 border-bottom mb-2">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group form-group-sm row text-sm mb-0">
+                            <label for="filter_kategori" class="col-md-1 col-form-label">Filter</label>
+                            <div class="col-md-3">
+                                <select name="filter_kategori" id="filter_kategori" class="form-control form-control-sm filter_kategori">
+                                    <option value="">- Semua -</option>
+                                    @foreach($kategori as $l)
+                                        <option value="{{ $l->kategori_id }}">{{ $l->kategori_nama }}</option>
+                                    @endforeach
+                                </select>
+                                <small class="form-text text-muted">Kategori Barang</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+
             <table class="table table-bordered table-striped table-hover table-sm" id="table_barang">
                 <thead>
                     <tr>
                         <th>ID</th>
                         <th>Kode</th>
                         <th>Nama Barang</th>
-                        <th>Kategori</th>
                         <th>Harga Beli</th>
                         <th>Harga Jual</th>
+                        <th>Kategori</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
             </table>
         </div>
     </div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" data-keyboard="false"
+        data-width="75%"></div>
 @endsection
 @push('css')
 @endpush
 @push('js')
     <script>
+        function modalAction(url = '') {
+            $('#myModal').load(url, function () {
+                $('#myModal').modal('show');
+            });
+        }
+        var tableBarang;
         $(document).ready(function () {
-            var dataBarang = $('#table_barang').DataTable({
+            tableBarang = $('#table_barang').DataTable({
+                processing: true,
                 serverSide: true,
                 ajax: {
-                    "url": "{{ route('barang.list') }}",
+                    "url": "{{ url('barang/list') }}",
                     "dataType": "json",
                     "type": "POST",
                     "data": function (d) {
-                        d.level_id = $('#kategori_id').val();
+                        d.filter_kategori = $('.filter_kategori').val();
                     }
                 },
                 columns: [{
                     data: "DT_RowIndex",
                     className: "text-center",
+                    width: "5%",
                     orderable: false,
                     searchable: false
                 }, {
                     data: "barang_kode",
                     className: "",
+                    width: "10%",
                     orderable: true,
                     searchable: true
                 }, {
                     data: "barang_nama",
                     className: "",
+                    width: "37%",
                     orderable: true,
-                    searchable: true
-                }, {
-                    data: "kategori.kategori_nama",
-                    className: "",
-                    orderable: false,
-                    searchable: false
+                    searchable: true,
                 }, {
                     data: "harga_beli",
-                    className: "text-right",
+                    className: "",
+                    width: "10%",
                     orderable: true,
                     searchable: false,
                     render: function (data, type, row) {
-                        return 'Rp ' + parseFloat(data).toLocaleString('id-ID');
+                        return new Intl.NumberFormat('id-ID').format(data);
                     }
                 }, {
                     data: "harga_jual",
-                    className: "text-right",
+                    className: "",
+                    width: "10%",
                     orderable: true,
                     searchable: false,
                     render: function (data, type, row) {
-                        return 'Rp ' + parseFloat(data).toLocaleString('id-ID');
+                        return new Intl.NumberFormat('id-ID').format(data);
                     }
+                }, {
+                    data: "kategori.kategori_nama",
+                    className: "",
+                    width: "14%",
+                    orderable: true,
+                    searchable: false
                 }, {
                     data: "aksi",
                     className: "text-center",
+                    width: "14%",
                     orderable: false,
                     searchable: false
-                }]
+                }
+                ]
             });
 
-            $('#filter_kategori').on('change', function () {
-                dataBarang.ajax.reload();
+            $('#table_barang_filter input').unbind().bind().on('keyup', function (e) {
+                if (e.keyCode == 13) { // enter key
+                    tableBarang.search(this.value).draw();
+                }
             });
+
+            $('.filter_kategori').change(function () {
+                tableBarang.draw();
+            });
+
+
         });
+
     </script>
 @endpush
