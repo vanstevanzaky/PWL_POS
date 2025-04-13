afd9543cb30770058b88707ada97089c19ae8412
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-04-13 20:13:53 +0700

    jbs8- praktikum 3: export pdf di laravel (modifikasi router barang)

--- routes/web.php
+++ routes/web.php
@@ -143,6 +143,7 @@ Route::middleware('auth')->group(function () {
             Route::get('/import', [BarangController::class, 'import'])->name('barang.import');
             Route::post('/import_ajax', [BarangController::class, 'import_ajax'])->name('barang.import_ajax');
             Route::get('/export_excel', [BarangController::class, 'export_excel'])->name('barang.export_excel');
+            Route::get('/export_pdf', [BarangController::class, 'export_pdf'])->name('barang.export_pdf');
         });
     });
 });
