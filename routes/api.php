30ad9621965ff1e3f7b9265c11f2ef87c0d94e82
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-04-18 16:10:27 +0700

    jbs10- praktikum 2: membuat restful api login (menambahkan kode pada route api.php untuk login)

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,8 @@ use App\Http\Controllers\Api\RegisterController;
 |
 */
 
-Route::post('register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
