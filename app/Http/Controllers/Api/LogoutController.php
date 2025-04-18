a102095625972e64b6e22a1c648c0c6bbeaf82fd
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-04-18 16:26:37 +0700

    jbs10- praktikum 3: membuat restful api logout (menambahkan kode pada LogoutController)

---
+++ app/Http/Controllers/Api/LogoutController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+class LogoutController extends Controller
+{
+    public function __invoke(Request $request) {
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        if ($removeToken) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout berhasil'
+            ]);
+        }
+    }
+}
