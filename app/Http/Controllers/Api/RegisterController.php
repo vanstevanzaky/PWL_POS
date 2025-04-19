73f63c4b278a2a3dd0187bed65e46a4dc652cfbe
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-04-19 21:58:56 +0700

    jbs11- praktikum 1: implementasi eloquent accessor (menambahkan hashName pada image)

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -23,12 +23,16 @@ class RegisterController extends Controller
             return response()->json($validator->errors(), 422);
         }
 
+        $image = $request->file('image');
+        $image->store('public/posts');
+
+
         $user = UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
             'password' => bcrypt($request->password),
             'level_id' => $request->level_id,
-            'image' => $request->image
+            'image' => $image->hashName()
         ]);
         if ($user) {
             return response()->json([
