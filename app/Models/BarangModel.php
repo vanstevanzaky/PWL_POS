7f6c4e585b22a7d5a4e888f2863d34b654d614a6
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-04-26 19:47:24 +0700

    jbs11- tugas: implementasi eloquent accessor (modifikasi BarangModel)

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -4,15 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject; 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
-class BarangModel extends Model
+
+class BarangModel extends Model implements JWTSubject
 {
     protected $table = 'm_barang';
      protected $primaryKey = 'barang_id';
-     protected $fillable = ['barang_id', 'kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
+     protected $fillable = ['barang_id', 'kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual','image'];
  
+     public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
      public function kategori()
      {
          return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+     }
+     protected function image(): Attribute
+     {
+         return Attribute::make(
+             get: fn($image) => url('/storage/posts/' . $image),
+         );
      }
 }
